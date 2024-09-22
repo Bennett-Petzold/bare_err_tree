ab180332893df1a28424a0ee8b4ee25e01f935c5
Bennett-Petzold <46354948+Bennett-Petzold@users.noreply.github.com>
2024-09-22 22:46:28 +0000

    Deploying to gh-pages from @ Bennett-Petzold/bare_err_tree@60e4c6394097ec811e54e04340469634b5f84136 🚀

--- docs/trait.impl/core/fmt/trait.Write.js
+++ docs/trait.impl/core/fmt/trait.Write.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["tracing_subscriber",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"tracing_subscriber/fmt/format/struct.Writer.html\" title=\"struct tracing_subscriber::fmt::format::Writer\">Writer</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["indenter",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"indenter/struct.Indented.html\" title=\"struct indenter::Indented\">Indented</a>&lt;'_, T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> + ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>,</div>"]]],["tracing_subscriber",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"tracing_subscriber/fmt/format/struct.Writer.html\" title=\"struct tracing_subscriber::fmt::format::Writer\">Writer</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[324]}
+//{"start":57,"fragment_lengths":[600,325]}
