11c181172776656375a7fe2c2e5540a5f5a84cac
Bennett Petzold <dansecob.dev@gmail.com>
2025-01-10 18:06:50 -0500

    Add separate JSON reconstruct; modify trace JSON format

--- bare_err_tree/tests/json.rs
+++ bare_err_tree/tests/json.rs
@@ -7,20 +7,22 @@
 ))]
 
 mod example {
-    include!("../test_cases/json/src/bin/example.rs");
+    use std::println;
+
+    include!("../test_cases/json/src/bin/reconstruct.rs");
 
     #[test]
     fn readme_example() {
-        let expected_json = "{\"msg\":\"missed class\",\"location\":\"bare_err_tree/tests/../test_cases/json/src/bin/example.rs:51:6\",\"trace\":[{\"target\":\"json::example\",\"name\":\"gen_print_inner\",\"fields\":\"\",\"location\":[\"bare_err_tree/tests/../test_cases/json/src/bin/example.rs\",38]}],\"sources\":[{\"msg\":\"stayed in bed too long\",\"location\":\"bare_err_tree/tests/../test_cases/json/src/bin/example.rs:40:57\",\"trace\":[{\"target\":\"json::example\",\"name\":\"new\",\"fields\":\"bed_time=BedTime { hour: 2, reasons: [FinishingProject(ClassProject { desc: \\\"proving 1 == 2\\\" }), ExamStressed, PlayingGames] } _garbage=5\",\"location\":[\"bare_err_tree/tests/../test_cases/json/src/bin/example.rs\",130]},{\"target\":\"json::example\",\"name\":\"gen_print_inner\",\"fields\":\"\",\"location\":[\"bare_err_tree/tests/../test_cases/json/src/bin/example.rs\",38]}],\"sources\":[{\"msg\":\"bed is comfortable\"},{\"msg\":\"went to sleep at 2 A.M.\",\"location\":\"bare_err_tree/tests/../test_cases/json/src/bin/example.rs:41:9\",\"trace\":[{\"target\":\"json::example\",\"name\":\"gen_print_inner\",\"fields\":\"\",\"location\":[\"bare_err_tree/tests/../test_cases/json/src/bin/example.rs\",38]}],\"sources\":[{\"msg\":\"finishing a project\",\"sources\":[{\"msg\":\"proving 1 == 2\"}]},{\"msg\":\"stressed about exams\"},{\"msg\":\"playing video games\"}]}]}]}";
+        let expected_json = "{\"msg\":\"missed class\",\"location\":\"bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:51:6\",\"trace\":[{\"target\":\"json::example\",\"name\":\"gen_print_inner\",\"fields\":\"\",\"source_loc\":[\"file\":\"bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs\",\"line\":38]}],\"sources\":[{\"msg\":\"stayed in bed too long\",\"location\":\"bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:40:57\",\"trace\":[{\"target\":\"json::example\",\"name\":\"new\",\"fields\":\"bed_time=BedTime { hour: 2, reasons: [FinishingProject(ClassProject { desc: \\\"proving 1 == 2\\\" }), ExamStressed, PlayingGames] } _garbage=5\",\"source_loc\":[\"file\":\"bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs\",\"line\":130]},{\"target\":\"json::example\",\"name\":\"gen_print_inner\",\"fields\":\"\",\"source_loc\":[\"file\":\"bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs\",\"line\":38]}],\"sources\":[{\"msg\":\"bed is comfortable\"},{\"msg\":\"went to sleep at 2 A.M.\",\"location\":\"bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:41:9\",\"trace\":[{\"target\":\"json::example\",\"name\":\"gen_print_inner\",\"fields\":\"\",\"source_loc\":[\"file\":\"bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs\",\"line\":38]}],\"sources\":[{\"msg\":\"finishing a project\",\"sources\":[{\"msg\":\"proving 1 == 2\"}]},{\"msg\":\"stressed about exams\"},{\"msg\":\"playing video games\"}]}]}]}";
 
         let expected_lines = r#"missed class
-├─ at bare_err_tree/tests/../test_cases/json/src/bin/example.rs:51:6
+├─ at bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:51:6
 │
 ├─ tracing frame 0 => json::example::gen_print_inner
-│        at bare_err_tree/tests/../test_cases/json/src/bin/example.rs:38
+│        at bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:38
 │
 ╰─▶ stayed in bed too long
-    ├─ at bare_err_tree/tests/../test_cases/json/src/bin/example.rs:40:57
+    ├─ at bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:40:57
     │
     ├─ tracing frame 1 => json::example::new with
     │    bed_time=BedTime {
@@ -36,13 +38,13 @@ mod example {
     │      ]
     │    }
     │    _garbage=5
-    │        at bare_err_tree/tests/../test_cases/json/src/bin/example.rs:130
+    │        at bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:130
     ├─ 1 duplicate tracing frame(s): [0]
     │
     ├─▶ bed is comfortable
     │
     ╰─▶ went to sleep at 2 A.M.
-        ├─ at bare_err_tree/tests/../test_cases/json/src/bin/example.rs:41:9
+        ├─ at bare_err_tree/tests/../test_cases/json/src/bin/reconstruct.rs:41:9
         │
         ├─ 1 duplicate tracing frame(s): [0]
         │
@@ -55,6 +57,15 @@ mod example {
         ╰─▶ playing video games"#;
 
         assert_eq!(gen_print(), expected_json);
+
+        for (expect, real) in reconstruct(&gen_print())
+            .lines()
+            .zip(expected_lines.lines())
+        {
+            assert_eq!(expect, real);
+            println!("{real}");
+        }
+
         assert_eq!(reconstruct(&gen_print()), expected_lines);
     }
 }
