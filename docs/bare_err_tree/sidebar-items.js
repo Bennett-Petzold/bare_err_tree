64a84ae4e062f33223b206f01286838e726dea73
Bennett-Petzold <46354948+Bennett-Petzold@users.noreply.github.com>
2025-01-28 03:49:07 +0000

    Deploying to gh-pages from @ Bennett-Petzold/bare_err_tree@b83c6c4b08f36bd58ddb07388aee9e3747b21205 🚀

--- docs/bare_err_tree/sidebar-items.js
+++ docs/bare_err_tree/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["err_tree"],"fn":["print_tree","reconstruct_output","tree_to_json","tree_unwrap"],"macro":["tree"],"mod":["flex"],"struct":["AdaptWrite","ErrTree","ErrTreePkg","InnerErrTreePkg"],"trait":["AsErrTree"]};
+window.SIDEBAR_ITEMS = {"attr":["err_tree"],"fn":["reconstruct_output","tree_to_json","tree_unwrap"],"macro":["tree"],"mod":["flex"],"struct":["ErrTree","ErrTreeDisplay","ErrTreePkg","InnerErrTreePkg"],"trait":["AsErrTree"]};
